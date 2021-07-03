06640b3a67b1610e1d2fa99da4891d588ca9facc
Fernando Guimaraes <ferguimara@gmail.com>
2021-07-03 19:24:20 -0400

    view all implemented

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#temporary data
+class Movie:
+    def __init__(self, title, release_year, rating):
+        self.title = title,
+        self.release_year = release_year,
+        self.rating = rating,
+
+movies = [
+    Movie('Inception', 2010, 8.7)
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello World')
+    return render(request, 'home.html')
+
+def about (request):
+    return render(request, 'about.html')
+
+def movies_index(request):
+    return render(request, 'movies/index.html', {'movies': movies})
