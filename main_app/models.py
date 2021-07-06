a3bd4ea3d715c2fb7d7092c30a6bdad441d630b4
Fernando Guimaraes <ferguimara@gmail.com>
2021-07-05 23:24:40 -0400

    added db and details

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100),
+    release_year = models.CharField(),
+    rating = models.FloatField(),
+
+    def __str__(self):
+        return self.title
--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-#temporary data
-class Movie:
-    def __init__(self, title, release_year, rating):
-        self.title = title,
-        self.release_year = release_year,
-        self.rating = rating,
-
-movies = [
-    Movie('Inception', 2010, 8.7)
-]
+from .models import Movie
 
 # Create your views here.
 def home(request):
@@ -20,4 +9,9 @@ def about (request):
     return render(request, 'about.html')
 
 def movies_index(request):
+    movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+def movies_detail(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
