7f77f2270605d5f7192fbbd98eabc93b65371cb9
Fernando Guimaraes <ferguimara@gmail.com>
2021-07-07 00:13:49 -0400

    full crud with CBV

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Movie(models.Model):
-    title = models.CharField(max_length=100),
-    release_year = models.CharField(),
-    rating = models.FloatField(),
+    title = models.CharField(max_length=100, default='hello')
+    release_year = models.CharField(max_length=4, default=2000)
+    rating = models.FloatField(default=5.0)
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'movie_id': self.id })
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import render
 from .models import Movie
 
@@ -15,3 +16,16 @@ def movies_index(request):
 def movies_detail(request, movie_id):
     movie = Movie.objects.get(id=movie_id)
     return render(request, 'movies/detail.html', {'movie': movie})
+
+class MovieCreate(CreateView):
+    model=Movie
+    fields = '__all__'
+
+class MovieUpdate(UpdateView):
+    model = Movie
+    #restrict from updating certain fields
+    fields = ['title', 'release_year', 'rating']
+
+class MovieDelete(DeleteView):
+    model = Movie
+    success_url = '/movies/'
